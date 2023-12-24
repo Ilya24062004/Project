b97a9bdc2c4e1e19772ff573d23a243adbf96d33
Roman <132680109+Selen1s@users.noreply.github.com>
2023-12-24 14:17:11 +0300

    Adding Fronted for website
    HTML+CSS (interaction with Java)

---
+++ Laba2/src/main/java/com/lr5/auto/AutoApplication.java
@@ -0,0 +1,10 @@
+package com.lr5.auto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AutoApplication.class, args);
+	}
+}
